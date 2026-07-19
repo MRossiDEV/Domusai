f98f34ac7dcf2eff3eae854cd2d04a2538d35ba4
Maurucio Rossi <mrossiph@gmail.com>
2026-07-19 17:59:52 -0300

    first commit

--- components/landing/FeaturedProperties.tsx
+++ components/landing/FeaturedProperties.tsx
@@ -54,7 +54,7 @@ export default function FeaturedProperties() {
             <PropertyCard featured {...properties[0]} />
           </div>
 
-          <div className="flex flex-row justify-between gap-6 mt-6">
+          <div className="flex flex-row flex-wrap justify-between gap-6 mt-6 lg:flex-nowrap">
             <PropertyCard {...properties[1]} />
             <PropertyCard  {...properties[2]} />
             <PropertyCard  {...properties[3]} />
